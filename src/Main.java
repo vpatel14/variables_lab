06357978586a2fc4ff00e5d91a4817722204aa16
Vedant Patel <100173804@lubbockisd.net>
2023-09-11 10:01:37 -0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,39 @@
+public class Main {
+    public static void main ( String[] args )
+    {
+//define 1 variable of each of the
+//following data types
+//byte short int long
+//float double
+//char boolean String
+//integer variables
+        byte byteOne = 127;
+        short shortOne = -32123;
+        int intOne = 90877;
+        long longOne = 999999999;
+        float floatOne = 38.5678F;
+        double doubleOne = 923.234;
+        String stringOne = "A";
+        boolean booleanOne = true;
+        String StringOne = "Hello world";
+
+        System.out.println("/////////////////////////////////");
+        System.out.println("*Some Person 07/18/02*");
+        System.out.println("* *");
+        System.out.println("* integer types *");
+        System.out.println("*8 bit - byteOne = "+byteOne+"\t\t        *");
+        System.out.println("*16 bit - shortOne = "+shortOne+"\t\t    *");
+        System.out.println("*32 bit - intOne = "+intOne+"\t\t    *");
+        System.out.println("*64 bit - longONe = "+longOne+"\t\t*\n");
+        System.out.println("*  real types                       *");
+        System.out.println("*32 bit - floatOne = "+floatOne+"\t\t*");
+        System.out.println("*64 bit - doubleOne = "+doubleOne+"\t\t*\n");
+        System.out.println("*other integer types*");
+        System.out.println("*16 bit - charOne = "+stringOne+"\t\t        *");
+
+        System.out.println("*Othertypes                         *");
+        System.out.println("*booleanOne = "+booleanOne+"\t\t            *\n");
+        System.out.println("*stringOne = "+StringOne+"\t\t    *");
+
+    }
+}
